2f1a47bc4850c05c2a078d364e835796810161c0
minhtriphan <39737967+minhtriphan@users.noreply.github.com>
2023-08-07 11:56:24 +0100

    Add files via upload

---
+++ tokenizer.py
@@ -0,0 +1,16 @@
+from transformers import AutoTokenizer
+from huggingface_hub import hf_hub_download
+
+class LongBERTTokenizer(AutoTokenizer):
+    def __init__(self):
+        super(AutoTokenizer, self).__init__()
+    
+    @classmethod
+    def from_pretrained(self, ckpt):
+        _special_tokens_map = hf_hub_download(repo_id = ckpt, filename = 'special_tokens_map.json')
+        _tokenizer = hf_hub_download(repo_id = ckpt, filename = 'tokenizer.json')
+        _tokenizer_config = hf_hub_download(repo_id = ckpt, filename = 'tokenizer_config.json')
+        _vocab = hf_hub_download(repo_id = ckpt, filename = 'vocab.txt')
+
+        path = '/'.join(_special_tokens_map.split('/')[:-1])
+        return AutoTokenizer.from_pretrained(path)
