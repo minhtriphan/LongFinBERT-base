0212e242c4879c4d85e0b9c169b351bd4ff03e5b
minhtriphan <39737967+minhtriphan@users.noreply.github.com>
2023-08-16 12:44:47 +0200

    Update tokenizer.py

--- tokenizer.py
+++ tokenizer.py
@@ -6,11 +6,11 @@ class LongBERTTokenizer(AutoTokenizer):
         super(AutoTokenizer, self).__init__()
     
     @classmethod
-    def from_pretrained(self, ckpt):
+    def from_pretrained(self, ckpt, **kwargs):
         _special_tokens_map = hf_hub_download(repo_id = ckpt, filename = 'special_tokens_map.json')
         _tokenizer = hf_hub_download(repo_id = ckpt, filename = 'tokenizer.json')
         _tokenizer_config = hf_hub_download(repo_id = ckpt, filename = 'tokenizer_config.json')
         _vocab = hf_hub_download(repo_id = ckpt, filename = 'vocab.txt')
 
         path = '/'.join(_special_tokens_map.split('/')[:-1])
-        return AutoTokenizer.from_pretrained(path)
+        return AutoTokenizer.from_pretrained(path, **kwargs)
